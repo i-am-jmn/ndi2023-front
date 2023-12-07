2d38999d3a12133a45b8a9ab59b3fefafffa7762
Jonathan MONTMAIN <jonathan.montmain@etu.univ-lyon1.fr>
2023-12-07 22:32:31 +0100

    feat: contenu de la page d'accueil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <v-sheet color="primary" class="py-16">
-    <v-container class="">
+    <v-container>
       <v-row align-content="space-between">
         <v-col cols="12" md="8" lg="6">
           <h1>Idécolo</h1>
@@ -12,6 +12,25 @@
           </v-sheet>
         </v-col>
       </v-row>
+    </v-container>
+  </v-sheet>
+  <v-sheet color="secondary" class="py-16">
+    <v-container>
+        <h2>Changer le monde commence par changer nos perceptions</h2>
+        <p>Il est facile de se laisser influencer par des idées reçues qui semblent décharger notre responsabilité individuelle. L'une d'entre elles affirme que "ce n'est pas à moi de jouer un rôle puisque les autres ne le font pas". Mais ici, nous croyons fermement que chaque action compte, chaque geste a un impact.</p>
+    </v-container>
+  </v-sheet>
+  <v-sheet color="secondary" class="py-16">
+    <v-container>
+        <h2>L'effet de nos actions individuelles est bien plus grand qu'on ne le pense</h2>
+        <p>Notre objectif est de démontrer que chaque pas vers des choix écologiques compte, que chaque voix qui se joint à la cause de la durabilité environnementale contribue à un changement collectif crucial.</p>
+    </v-container>
+  </v-sheet>
+  <v-sheet color="secondary" class="py-16">
+    <v-container>
+        <h2>Soyons des acteurs du changement, pas des spectateurs</h2>
+        <p>Sur ce site, vous découvrirez des faits, des histoires inspirantes et des informations pour démystifier les idées fausses sur l'écologie. Nous vous invitons à explorer, à vous informer et à agir, car chaque personne qui s'engage fait avancer notre planète vers un avenir meilleur.</p>
+        <p>Rejoignez-nous dans notre quête pour dépasser les idées reçues et pour être une force de changement pour notre planète !</p>
     </v-container>
   </v-sheet>
 </template>
