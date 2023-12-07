eaeaff4b5bb4ee1b13c4cb0e00d3b4098abf584d
Jonathan MONTMAIN <jonathan.montmain@etu.univ-lyon1.fr>
2023-12-07 19:24:16 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-nav-icon />
+      <v-app-bar-title>Idécolo</v-app-bar-title>
+    </v-app-bar>
+    <router-view />
+  </v-app>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid :style="{ backgroundColor: $vuetify.theme.current.colors.info }">
+    <v-container>
+      <v-row align-content="space-between">
+        <v-col cols="12" md="8" lg="6">
+          <h1>Idécolo</h1>
+          <p>Accepter de challengez votre point de vue.</p>
+        </v-col>
+        <v-col cols="12" md="4" lg="6">
+          <v-row align-content="center">
+            <v-btn>Se défier !</v-btn>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
+</template>
