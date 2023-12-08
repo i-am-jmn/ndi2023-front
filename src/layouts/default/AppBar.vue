55eeba13fe399a179910f065ec86ee7b16e5a93b
Claire Fernandes <claire.fernandes@etu.univ-lyon1.fr>
2023-12-08 01:08:21 +0100

    homePage v1

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,5 +1,56 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>Idécolo</v-app-bar-title>
+  <v-app-bar
+    color="#8BD93D"
+    prominent
+  >
+    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title>Idécolo</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn variant="text" icon="mdi-magnify"></v-btn>
+    <v-btn variant="text" icon="mdi-filter"></v-btn>
+    <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
   </v-app-bar>
+
+  <v-navigation-drawer
+    v-model="drawer"
+    location="left"
+    temporary
+  >
+    <v-list
+      :items="items"
+    ></v-list>
+  </v-navigation-drawer>
 </template>
+
+<script lang="ts">
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+      items: [
+        {
+          title: 'Foo',
+          value: 'foo',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        },
+        {
+          title: 'Fizz',
+          value: 'fizz',
+        },
+        {
+          title: 'Buzz',
+          value: 'buzz',
+        },
+      ],
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+</script>
